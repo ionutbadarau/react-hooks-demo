df1bf9646a0a963aa5dca878af3fa9ae8f38b562
Ioan Badarau <ioan.badarau@softvision.ro>
2020-01-13 17:33:17 +0200

    nitial comm

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
+    'react/jsx-filename-extension': 0,
+    "react/jsx-one-expression-per-line": 0,
+  }
+};
---
+++ src/components/example.js
@@ -0,0 +1,19 @@
+const express = 1;
+
+const a = 'adsa';
+
+console.log('aasda');
+
+
+const obj = {
+  hello: function () {
+    console.log('hello');
+  },
+};
+
+
+class A {
+  foo() {
+    console.log('Hello World'); /* error Expected 'this' to be used by class method 'foo'. */
+  }
+}
